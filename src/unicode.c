287597958277088d4f466e779bc4a14352344821
wt5v <wt5v@outlook.com>
2019-02-01 18:32:29 -0600

    Added a unicode example for real this time.

---
+++ src/unicode.c
@@ -0,0 +1,30 @@
+/**
+ * unicode.c
+ */
+
+#define _XOPEN_SOURCE_EXTENDED 1
+#include <curses.h>
+#include <locale.h>
+
+int main()
+{
+    cchar_t heart = {
+	A_NORMAL,
+	L"\u2665"
+    };
+
+    setlocale(LC_CTYPE, "en_US.UTF-8");
+
+    initscr();
+
+    addstr("I ");
+    add_wch(&heart);
+    addstr(" Ncurses!\n");
+
+    refresh();
+    getch();
+
+    endwin();
+    return(0);
+}
+
